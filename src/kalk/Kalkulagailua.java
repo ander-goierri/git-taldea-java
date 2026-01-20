a610afa58c5ee2de434b0e8a54d4a7e7e67c9537
Ander <amadinabeitia.goierri@gmail.com>
2026-01-20 12:49:31 +0100

    Biderketa gehituta eta main-a aldatuta

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -2,8 +2,13 @@ package kalk;
 
 public class Kalkulagailua {
 	
-	public static int zatiketa(int zatidura, int zatitzailea) {
-		return zatidura/zatitzailea;
+	public static int zatiketa(int a, int b) {
+		return a/b;
 	}
+	
+	public static int biderketa(int a, int b) {
+		return a*b;
+	}
+	
 	
 }
--- src/kalk/Main.java
+++ src/kalk/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(Kalkulagailua.zatiketa(10, 2));
+		int zenb1 = 10;
+		int zenb2 = 2;
+		System.out.println(Kalkulagailua.zatiketa(zenb1, zenb2));
+		System.out.println(Kalkulagailua.biderketa(zenb1, zenb2));
 		
 	}
 
