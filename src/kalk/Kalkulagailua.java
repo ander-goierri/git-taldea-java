262f00e5cd903aa5a27035c9224163a691ced756
alexnicolas-goierri <ale_mar_@goierrieskola.org>
2026-01-21 09:02:57 +0100

    4 revert proba

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -4,11 +4,6 @@ public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
 		return zatidura/zatitzailea;
-		if (zatitzailea ==0) {
-			System.out.println("Emaitza 0da");
-			
-		}
-		return zatidura/zatitzailea;
 	}
 	
 	public static int zatiketaZeroKontuanEdukita(int zatidura, int zatitzailea) {
--- src/kalk/Main.java
+++ src/kalk/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
+<<<<<<< HEAD
 		System.out.println(Kalkulagailua.zatiketa(10,0));
-		System.out.println(Kalkulagailua.zatiketa(11,4));
+=======
+		System.out.println(Kalkulagailua.zatiketa(10, 2));
+>>>>>>> parent of e260e32 (0 zatidura)
 		
 	}
 
