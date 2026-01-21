05b44c60819cf00cea69466623ccd2f41df7cae8
OierCalvo <oie_cal_arr@goierrieskola.org>
2026-01-21 08:47:22 +0100

    5.ariketa

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -4,6 +4,7 @@ public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
 		return zatidura/zatitzailea;
+		//ez nuen ulertzen:4.	Commit bat egin branch horretan, zatiketa konponduta, ziurtatu zatitzailea 0ren ezberdina dela. 0 den kasuetan emaitza ere 0 izan beharko da. Kodea ere erakutsi argazkian.
 	}
 	
 }
