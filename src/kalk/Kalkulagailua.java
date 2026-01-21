b6ceca9e0fcf7755f860e3044b01b46ef9e7da1d
Daniel Pedregosa <dan_ped_jim@goierrieskola.org>
2026-01-21 08:38:21 +0100

    Zatiketa konponduta

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -4,6 +4,11 @@ public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
 		return zatidura/zatitzailea;
+		if(zatitzailea!=0) {
+			return zatidura/zatitzailea;
+		}else {
+			return 0;
+		}
 	}
 	
 }
