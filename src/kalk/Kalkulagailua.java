905eb92d4e045008bf77ac675f723c5f0d92d6b1
inigo proba bi <inigo@proba.com>
2026-01-21 08:49:22 +0100

    zatitzailea o denean emaitza 0

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -4,6 +4,9 @@ public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
 		return zatidura/zatitzailea;
+		if (zatitzailea = 0 ) {
+			System.out.println("emaitza 0 da");
+		}
 	}
 	
 }
