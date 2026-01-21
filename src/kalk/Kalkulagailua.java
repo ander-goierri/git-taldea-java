da228dbb2681d4684924d08dd72df36b34f2f3c6
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-01-21 09:19:17 +0100

    Revert "Revert eginda"
    This reverts commit e9c5e20933ad9410f28cb094db30079329f2fe7b.

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -1,20 +1,15 @@
 package kalk;
 
 public class Kalkulagailua {
-	int emaitza=0;
 	
 	public static int zatiketa(int zatidura, int zatitzailea, int emaitza) {
-		emaitza=zatidura/zatitzailea;
+		return emaitza=zatidura/zatitzailea;
 		if (zatitzailea!=0) {
 			emaitza=emaitza;
 		}else{
 			emaitza=0;
 			
 		}
-		return emaitza;
-	}
-	public int zatiketaZeroKontutanEdukita() {
-		return zatiketa(emaitza, emaitza, emaitza);
 	}
 	
 }
