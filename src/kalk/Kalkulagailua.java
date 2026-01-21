e260e32008fc1cf524ddef55af5f9c87344861e3
alexnicolas-goierri <ale_mar_@goierrieskola.org>
2026-01-21 08:55:16 +0100

    0 zatidura

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -4,6 +4,11 @@ public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
 		return zatidura/zatitzailea;
+		if (zatitzailea ==0) {
+			System.out.println("Emaitza 0da");
+			
+		}
+		return zatidura/zatitzailea;
 	}
 	
 }
