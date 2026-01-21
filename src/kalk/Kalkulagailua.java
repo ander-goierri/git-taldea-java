cf1f9c90995170dda4792e6250aa421c67d5fc37
inigo proba bi <inigo@proba.com>
2026-01-21 08:56:04 +0100

    kalkulagailua eta main aldaketak eginda

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -8,5 +8,11 @@ public class Kalkulagailua {
 			System.out.println("emaitza 0 da");
 		}
 	}
+	public static int zatiketaZeroKontutanEdukita(int zatidura, int zatitzailea) {
+		return zatidura/zatitzailea;
+		if (zatitzailea = 0 ) {
+			System.out.println("emaitza 0 da");
+		}
+	}
 	
 }
--- src/kalk/Main.java
+++ src/kalk/Main.java
@@ -1,10 +1,17 @@
 package kalk;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(Kalkulagailua.zatiketa(10, 2));
+	
+        System.out.println("Sartu lehenengo zebakia");
+        String batZen = sc.nextLine();
+        System.out.println("Sartu bigarren zebakia");
+        String biZen = sc.nextLine();
+        
 		
 	}
 
