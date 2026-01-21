bac1abce0f006542086920c123613d72107cb911
Kimetz Milan <kim_mil_apa@goierrieskola.org>
2026-01-21 08:49:34 +0100

    kalkulagailuakonponduta

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -2,8 +2,11 @@ package kalk;
 
 public class Kalkulagailua {
 	
+	
 	public static int zatiketa(int zatidura, int zatitzailea) {
-		return zatidura/zatitzailea;
+		if (zatitzailea = 0) {
+			System.out.println("ezin da egin zatiketa");
+		}else return zatidura/zatitzailea;
 	}
 	
 }
