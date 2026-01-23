3af28a42a5f4346c67d7f679caf7ba762c5160f6
Aimar <aim.zab.jau@goierrieskola.org>
2026-01-23 09:07:26 +0100

    zatiketa konponduta

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -3,6 +3,9 @@ package kalk;
 public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
+		if(zatitzailea == 0) {
+		return 0;
+		}
 		return zatidura/zatitzailea;
 	}
 	
