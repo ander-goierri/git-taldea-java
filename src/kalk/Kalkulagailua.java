691a98245f0a0473990cf3b5fe8523a9247645fe
Ander Urien Goierri <and_uri_tel@goierrieskola.org>
2026-05-25 23:00:14 +0200

    4.ARIKETA: zatiketa konpondu (if gehituz), eta commit egin

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -3,7 +3,14 @@ package kalk;
 public class Kalkulagailua {
 	
 	public static int zatiketa(int zatidura, int zatitzailea) {
+		if (zatitzailea == 0) {
+			System.out.println("Ezin da zeroz zatitu, INDETERMINAZIOA");
+			return 0;
+		}
+
 		return zatidura/zatitzailea;
 	}
+
+	
 	
 }
