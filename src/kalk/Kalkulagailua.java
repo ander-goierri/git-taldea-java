d8429a9e6ec5d84d359bfb7585d6b582f2cb7eb3
Ander Urien Goierri <and_uri_tel@goierrieskola.org>
2026-05-25 23:13:03 +0200

    6.ARIKETA: 4.arikerako comita ezbatu

--- src/kalk/Kalkulagailua.java
+++ src/kalk/Kalkulagailua.java
@@ -2,15 +2,13 @@ package kalk;
 
 public class Kalkulagailua {
 	
+
 	public static int zatiketaZeroKontutanEdukita(int zatidura, int zatitzailea) {
 		if (zatitzailea == 0) {
 			System.out.println("Ezin da zeroz zatitu, INDETERMINAZIOA");
 			return 0;
 		}
-
-		return zatidura/zatitzailea;
+		return zatidura / zatitzailea;
 	}
-
-	
-	
 }
+
